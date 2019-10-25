ef106fe6c7e08f6a41b36b600c09e1f4ab7f8a55
Tom Galligan <tom.galligan@beamery.com>
2019-10-25 13:10:44 +0100

    add link to external sites functionality

--- src/components/CardBanner/CardBanner.js
+++ src/components/CardBanner/CardBanner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./CardBanner.module.css";
+import linkIconPath from "./link.svg";
 const CardBanner = ({ mediaType, cardSourceURL }) => {
 	return (
 		<div
@@ -8,7 +9,12 @@ const CardBanner = ({ mediaType, cardSourceURL }) => {
 				" " +
 				(mediaType === "tweet" ? styles.twitter : styles.news)
 			}
-		></div>
+		>
+			{" "}
+			<a href={cardSourceURL}>
+				<img src={linkIconPath} className={styles.icon} />
+			</a>
+		</div>
 	);
 };
 
