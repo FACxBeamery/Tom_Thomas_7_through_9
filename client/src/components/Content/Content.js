a7cb53bca687219ff625ca2652c1f471b53709ab
thomas-galligan <tom.galligan@beamery.com>
2019-11-07 11:41:56 +0000

    add api request functions

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -2,140 +2,77 @@ import React from "react";
 import { Card } from "./Card/Card";
 
 import styles from "./Content.module.css";
-// import twitterClient from "./twitterConfig.js";
+
 
 const Content = ({ contentChoice, tweetsSelected, newsSelected, data }) => {
-	// const [newsArticles, setNewsArticles] = React.useState(null);
+    const [mockNews, mockTweet] = data;
 
-	// const twitterSearchParams = {
-	// 	q: "beamery",
-	// 	count: 10,
-	// 	result_type: "recent",
-	// 	lang: "en"
-	// };
+    mockNews.articles.forEach((elem) => {
+        elem.mediaType = "news";
+        elem.dateStandard = new Date(elem.publishedAt).toUTCString();
+    });
 
-	// twitterClient.get(
-	// 	"search/tweets",
-	// 	twitterSearchParams,
-	// 	(error, data, response) => {
-	// 		if (!error) {
-	// 			console.log(JSON.parse(response.body));
-	// 		} else {
-	// 			console.error(error);
-	// 		}
-	// 	}
-	// );
+    mockTweet.statuses.forEach((elem) => {
+        elem.mediaType = "tweet";
+        elem.dateStandard = new Date(elem.created_at).toUTCString();
+    });
 
-	// const googleNewsApiKey = process.env.REACT_APP_GOOGLE_NEWS_API_KEY;
+    const filteredMedia = [];
 
-	// const getGoogleNews = () => {
-	// 	fetch(
-	// 		`https://newsapi.org/v2/everything?q=beamery&language=en&sortBy=publishedAt&apiKey=${googleNewsApiKey}`
-	// 	)
-	// 		.then((response) => {
-	// 			if (response.status !== 200) {
-	// 				throw new Error(
-	// 					"An error occurred when retrieving articles relating to Beamery, using the Google News API."
-	// 				);
-	// 			}
-	// 			return response.json();
-	// 		})
-	// 		.then((response) => response.articles)
-	// 		.then((articles) => {
-	// 			const cleanedNewsArticles = articles.map((elem) => {
-	// 				const cleanedArticle = {};
-	// 				cleanedArticle.imageUrl = elem.urlToImage;
-	// 				cleanedArticle.title = elem.title;
-	// 				cleanedArticle.heading = elem.source.name;
-	// 				cleanedArticle.mainText = elem.description;
-	// 				cleanedArticle.sourceUrl = elem.url;
-	// 				cleanedArticle.mediaType = "news";
-	// 				cleanedArticle.dateStandard = new Date(
-	// 					elem.publishedAt
-	// 				).toUTCString();
+    if (tweetsSelected) {
+        filteredMedia.push(...mockTweet.statuses);
+    }
+    if (newsSelected) {
+        filteredMedia.push(...mockNews.articles);
+    }
 
-	// 				return cleanedArticle;
-	// 			});
-	// 			setNewsArticles(cleanedNewsArticles);
-	// 			// console.log("cleanArt: ", cleanedNewsArticles);
-	// 		})
-	// 		.catch((error) => {
-	// 			throw new Error(error.message);
-	// 		});
-	// };
-
-	// React.useEffect(() => {
-	// 	getGoogleNews();
-	// }, []);
-	// console.log(data);
-	const [mockNews, mockTweet] = data;
-
-	mockNews.articles.forEach((elem) => {
-		elem.mediaType = "news";
-		elem.dateStandard = new Date(elem.publishedAt).toUTCString();
-	});
-
-	mockTweet.statuses.forEach((elem) => {
-		elem.mediaType = "tweet";
-		elem.dateStandard = new Date(elem.created_at).toUTCString();
-	});
-
-	const filteredMedia = [];
-
-	if (tweetsSelected) {
-		filteredMedia.push(...mockTweet.statuses);
-	}
-	if (newsSelected) {
-		filteredMedia.push(...mockNews.articles);
-	}
-
-	if (filteredMedia.length === 0) {
-		return (
-			<>
-				<p className={styles["no-content"]}>
-					Please select the content you'd like to see
+    if (filteredMedia.length === 0) {
+        return (
+            <>
+                <p className={styles["no-content"]}>
+                    Please select the content you'd like to see
 				</p>
-			</>
-		);
-	}
+            </>
+        );
+    }
 
-	filteredMedia.sort((a, b) => {
-		const dateA = new Date(a.dateStandard);
-		const dateB = new Date(b.dateStandard);
-		return dateB - dateA;
-	});
+    filteredMedia.sort((a, b) => {
+        const dateA = new Date(a.dateStandard);
+        const dateB = new Date(b.dateStandard);
+        return dateB - dateA;
+    });
 
-	// const cardsToRender = filteredMedia.map((element) => {
-	// 	console.log("inside cardsToRender, logging each card: ", element);
-	// 	return <Card cardData={element} />;
-	// });
+    // const cardsToRender = filteredMedia.map((element) => {
+    // 	console.log("inside cardsToRender, logging each card: ", element);
+    // 	return <Card cardData={element} />;
+    // });
 
-	const cardsToRender =
-		contentChoice === "most-recent"
-			? filteredMedia.map((element) => {
-					return (
-						<Card
-							key={Date.parse(element.dateStandard)}
-							cardData={element}
-						/>
-					);
-			  })
-			: filteredMedia
-					.filter((element) => element.favourite === true)
-					.map((element) => {
-						return (
-							<Card
-								key={Date.parse(element.dateStandard)}
-								cardData={element}
-							/>
-						);
-					});
+    const cardsToRender =
+        contentChoice === "most-recent"
+            ? filteredMedia.map((element) => {
+                return (
+                    <Card
+                        key={Date.parse(element.dateStandard)}
+                        cardData={element}
+                    />
+                );
+            })
+            : filteredMedia
+                .filter((element) => element.favourite === true)
+                .map((element) => {
+                    return (
+                        <Card
+                            key={Date.parse(element.dateStandard)}
+                            cardData={element}
+                        />
+                    );
+                });
 
-	return (
-		<div className={styles["content"]} data-testid="content-container">
-			{[cardsToRender]}
-		</div>
-	);
+    return (
+        <div className={styles["content"]} data-testid="content-container">
+            {[cardsToRender]}
+        </div>
+    );
 };
 
 export default Content;
--- server/router.js
+++ server/router.js
@@ -2,9 +2,31 @@ const express = require("express");
 const router = express();
 
 // import handlers here...
+let lastReqTime;
 
-router.get("/media", mediaHandler);
+router.get("/media", () => mediaHandler(lastReqTime));
 
 router.patch("/media/:id", favouriteHandler);
 
 module.exports = router;
+
+const mediaHandler = (lastReqTime) => {
+    if (Date.now() - lastReqTime > 60000) {// 1 minute
+        const twitterApiResponse = twitterApi();
+        const cleanTweets = cleanTwitterApiResponse(twitterApiResponse);
+
+        const news = googleNewsApi();
+        populateDBQuery([cleanTweets, news]);
+
+    }
+    return getDBQuery(); // send this to front end
+};
+
+const APITwitter = () => {
+    // get response from twitter, clean up and return array of tweets
+};
+
+const populateDBQuery = (media) => {
+    // check each unique ID in media against IDs currently in DB. Do not 
+    // add any items already present. 
+};
