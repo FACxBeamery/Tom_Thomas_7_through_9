0a821aa740b90bc472d60484f942a7b1349aa19e
thomas-galligan <tom.galligan@beamery.com>
2019-11-08 11:14:28 +0000

    try to fix proxy bug

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,24 @@ import mockNews from "./mockNews";
 import mockTweet from "./mockTweet";
 import AppChild from "./AppChild";
 
-const data = [mockNews, mockTweet];
 
 const App = () => {
-	let data;
-	React.useEffect(() => {
-		async function getMessage() {
-			try {
-				const response = await fetch("/media");
-				console.log("response :", response);
-				data = response.data;
-			} catch (err) {
-				console.log("Error inside useEffect.");
-				console.log(err);
-			}
-		}
-		getMessage();
-	}, []);
-	console.log("data:", data);
-	return <AppChild data={data} />;
+    const [data, setData] = React.useState([mockNews, mockTweet]);
+    React.useEffect(() => {
+        async function getMessage() {
+            try {
+                const response = await fetch("/media");
+                console.log("response :", response);
+                setData(response.data);
+            } catch (err) {
+                console.log("Error inside useEffect.");
+                console.log(err);
+            }
+        }
+        getMessage();
+    }, []);
+    console.log("data:", data);
+    return <AppChild data={data} />;
 };
 
 export default App;
--- server/api/twitterConfig.js
+++ server/api/twitterConfig.js
@@ -1,10 +1,10 @@
 const Twitter = require("twitter");
 
 const twitterClient = new Twitter({
-    consumer_key: "pHk7rp7J0sZK99CavAZs9fe6o", // process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: "hn0bWg9DvVtuHOl8lT0qLHmnMbMPKhoSE4qCiA7IDyuzafSmfO", //process.env.TWITTER_CONSUMER_KEY_SECRET,
-    access_token_key: "1164596432860602370-U6bsnduOROemRkhPpBLK1kEZecmRgA", //process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: "UaDJYO7vGFgfAuWdimlsthxPm0qSLjSVcJip6RpRmEKZC" //process.env.TWITTER_ACCESS_TOKEN_SECRET
+    consumer_key: "G4tPW1DmGMY7gDwnBKBQiJNBU", // process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: "meW4ZQOkkWiCIvlvFtoYFhNpmZiSxU97rhpzrQYtd6tKnRf6tg", //process.env.TWITTER_CONSUMER_KEY_SECRET,
+    access_token_key: "1164596432860602370-8eTIjzUK4k0yUh64boEJJeeZM7rGXz", //process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: "JkqIjh8xIn1bS1zDsTCk19SIsvoXIflV9bDwJ0WLGH0Lj" //process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 module.exports = twitterClient;
